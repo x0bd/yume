a17168d54084e7737410ee44af6a250b610fc240
Tinodaishe Tembo <synthmancer@gmail.com>
2024-07-04 22:03:34 +0200

    wip new job post form ui

--- app/components/JobForm.tsx
+++ app/components/JobForm.tsx
@@ -7,6 +7,8 @@ import {
 	StateSelect,
 } from "react-country-state-city";
 import "react-country-state-city/dist/react-country-state-city.css";
+import { RiImage2Line } from "react-icons/ri";
+import { MdOutlinePerson } from "react-icons/md";
 
 export default function JobForm() {
 	const [countryId, setCountryId] = useState(0);
@@ -14,7 +16,7 @@ export default function JobForm() {
 
 	return (
 		<form
-			className="p-2 mt-8 flex flex-col gap-3 rounded bg-gray-200 "
+			className="p-3 mt-8 flex flex-col gap-3 rounded bg-gray-200 "
 			action=""
 		>
 			<h3 className="text-sm font-black">New Job Post</h3>
@@ -22,6 +24,7 @@ export default function JobForm() {
 				className="focus:outline-none text-gray-600 rounded w-full p-3 text-sm border-none"
 				placeholder="Job Title"
 			/>
+
 			<div className="grid md:lg:grid-cols-2 gap-2">
 				<div className="flex gap-3">
 					<label className="text-sm text-gray-800" htmlFor="location">
@@ -88,10 +91,47 @@ export default function JobForm() {
 					/>
 				</div>
 			</div>
+
 			<textarea
-				className="p-3 focus:outline-none text-gray-600 rounded-sm text-sm w-full border-none"
+				className="p-3 h-56 focus:outline-none text-gray-600 rounded-sm text-sm w-full border-none"
 				placeholder="Job Description..."
 			/>
+
+			<div className="flex lg:md:flex-row flex-col lg:md:gap-2">
+				<div className="flex flex-col w-2/3 gap-2">
+					<h3 className="text-sm font-medium text-gray-800">
+						Contact Person
+					</h3>
+					<div className="flex gap-2 w-full grow items-center">
+						<div className="bg-gray-100 size-24 rounded cursor-pointer  flex items-center justify-center">
+							<MdOutlinePerson size={40} />
+						</div>
+						<div className="flex flex-col grow w-auto gap-1">
+							<input
+								type="text"
+								placeholder="Jane Doe"
+								className="border w-full p-1 rounded text-sm text-gray-600 focus:outline-none border-none"
+							/>
+							<input
+								type="text"
+								placeholder="Phone"
+								className="border p-1 w-full rounded text-sm text-gray-600 focus:outline-none border-none"
+							/>
+							<input
+								type="text"
+								placeholder="Email"
+								className="border p-1 w-full rounded text-sm text-gray-600 focus:outline-none border-none"
+							/>
+						</div>
+					</div>
+				</div>
+				<div className="flex flex-col gap-2">
+					<h3 className="text-sm text-gray-800">Job Icon</h3>
+					<div className="bg-gray-100 size-24 rounded cursor-pointer flex items-center justify-center">
+						<RiImage2Line size={40} />
+					</div>
+				</div>
+			</div>
 		</form>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,7 @@
-import Link from "next/link";
-import {
-	getSignInUrl,
-	getSignUpUrl,
-	getUser,
-	signOut,
-} from "@workos-inc/authkit-nextjs";
-
-import Image from "next/image";
 import Hero from "./components/Hero";
 import Jobs from "./components/Jobs";
-import Footer from "./components/Footer";
 
 export default async function Home() {
-	// Retrieves the user from the session or returns `null` if no user is signed in
-	const { user } = await getUser();
-
-	// Get the URL to redirect the user to AuthKit to sign in
-	const signInUrl = await getSignInUrl();
-
-	// Get the URL to redirect the user to AuthKit to sign up
-	const signUpUrl = await getSignUpUrl();
 	return (
 		<>
 			<Hero />
